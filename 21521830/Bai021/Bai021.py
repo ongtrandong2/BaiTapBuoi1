8e4735ed85ca9fd2ba65ed877792efa050c7b578
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2023-09-15 18:27:57 +0700

    21521830

---
+++ 21521830/Bai021/Bai021.py
@@ -0,0 +1,5 @@
+x = float(input('Nhap x: '))
+x2 = x * x
+x5 = x2 * x2 * x
+x15 = x5 * x5 * x5
+print('x15 = ', x15)
---
+++ 21521830/Bai039/Bai039.py
@@ -0,0 +1,7 @@
+n = int(input("Nhap n: "))
+s = 1
+i = 1
+while (i <= n):
+    s = s * (1 + float(1 / (i * i)))
+    i = i + 1
+print("S(", n, ") = ", s)
