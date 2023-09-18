eb619685db7876769f99202cec5c51a2c1c6626b
TTinhCaCa <frankenstein1909@gmail.com>
2023-09-18 10:17:27 +0700

    nop bai

---
+++ 21521957/Bai018/Bai018.py
@@ -0,0 +1,12 @@
+﻿
+x  = float(input("Nhập giá trị của x: "))
+
+x_first = x * x
+
+x_second = x_first * x_first
+
+x_third = x_second * x_second
+
+x_fourth = x_third * x_second
+
+print("Kết quả: ",x_fourth)
